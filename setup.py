e06cadcb170754f11784931b7ca9ab1fd6732dcb
Dohyung Park <dohyung.prk@gmail.com>
2021-03-14 01:09:26 +0900

    :package: add classifiers

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ from setuptools import find_packages, setup
 setup(
     name='airpan',
     version='0.0.1',
+    python_requires=">=3.5",
+    classifiers=[
+        "OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     description='Airflow operator and sensor plugins',
     long_description=open('README.md').read(),
     url='https://github.com/dohyungp/airfan',
